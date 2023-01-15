7e5a196a0ad3561a0a9f846507c3c6f2895f31b5
Tusko <tusko@photoinside.me>
2023-01-15 09:10:59 +0200

    try serverless

--- src/app.js
+++ src/app.js
@@ -1,27 +1,39 @@
+// const SmartPlug = require("./SmartPlug");
+// const fastify = require("fastify")
+
+// function init() {
+//   const app = fastify();
+//   app.get("/", (_, reply) => {
+//     const smartPlug = SmartPlug();
+
+//     reply.send({
+//       hello: smartPlug,
+//     });
+//   });
+//   return app;
+// }
+
+// console.log(require.main === module)
+
+// if (require.main !== module) {
+//   // called directly i.e. "node app"
+//   init().listen(3000, (err) => {
+//     if (err) console.error(err);
+//     console.log("server listening on 3000");
+//   });
+// } else {
+//   // required as a module => executed on aws lambda
+//   module.exports = init;
+// }
+
 const SmartPlug = require("./SmartPlug");
-const fastify = require("fastify")
+const fastify = require("fastify");
+const app = fastify();
+const serverless = require("serverless-http");
 
-function init() {
-  const app = fastify();
-  app.get("/", (_, reply) => {
-    const smartPlug = SmartPlug();
+app.get("/", (_, reply) => {
+  const sm = SmartPlug();
+  reply.send({hello: sm});
+});
 
-    reply.send({
-      hello: smartPlug,
-    });
-  });
-  return app;
-}
-
-console.log(require.main === module)
-
-if (require.main !== module) {
-  // called directly i.e. "node app"
-  init().listen(3000, (err) => {
-    if (err) console.error(err);
-    console.log("server listening on 3000");
-  });
-} else {
-  // required as a module => executed on aws lambda
-  module.exports = init;
-}
+module.exports.handler = serverless(app);
